08aa1de4e05a44f5402f826290aa5659ce4cede9
efruchter <fruchter.eric@gmail.com>
2011-08-18 00:58:08 -0400

    Added a nifty icon

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.Toolkit;
 import java.io.File;
 
 import javax.swing.JFrame;
@@ -23,6 +24,8 @@ public class Main {
 
 		final JFrame frame = new JFrame("Gnome 3 Application Menu Editor");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(
+				"images/icon.png"));
 		frame.setResizable(false);
 		frame.add(new FileListPanel(new File(ENTRY_DIRECTORY)));
 		frame.pack();
