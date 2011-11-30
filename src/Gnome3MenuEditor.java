8f8d5e7a653e34bd509c3c542106fbb190cdb5b1
efruchter <fruchter.eric@gmail.com>
2011-11-30 02:16:38 -0500

    A little refactoring in main

--- src/Gnome3MenuEditor.java
+++ src/Gnome3MenuEditor.java
@@ -12,23 +12,56 @@ import javax.swing.JFrame;
  */
 public class Gnome3MenuEditor {
 
-	public static String entryDirectorySuffix = "/.local/share/applications/";
+	/**
+	 * The usual path from within HOME to get to the application entries.
+	 */
+	public static final String ENTRY_DIR_FROM_HOME = "/.local/share/applications/";
 
-	public static JFrame frame;
+	/**
+	 * The entry directory.
+	 */
+	private static String dir = ".";
 
 	public static void main(final String[] args) {
+		if (args.length == 0) {
+			String username = null;
+			try {
+				username = System.getProperty("user.name");
+			} catch (final SecurityException e) {
+				System.err
+						.println("Unable to access user.name value! Please supply a directory with -d");
+				System.exit(0);
+			}
+			if (username == null) {
+				System.err
+						.println("No value for the system property user.name! Please supply a directory with -d");
+				System.exit(0);
+			}
+			dir = "/home/" + username + ENTRY_DIR_FROM_HOME;
+		} else if (args[0].contains("-h") || args[0].contains("-help")) {
+			System.out
+					.println("This editor allows you to edit the entries in /usr/share/applications/, "
+							+ "which controls the Gnome 3 Applications Menu. "
+							+ "To specify a different directory to parse for entries, "
+							+ "pass it in through command line with '-d'. ex: 'sudo java -jar gnome3-menu-editor -d /home/username/Desktop'.");
+			System.exit(0);
+		} else if (args[0].contains("-d")) {
+			if (args.length == 1) {
+				System.out.println("Please supply a directory along with -d");
+				System.exit(0);
+			}
+			dir = args[1];
+		}
 
-		checkForTerminalCommands(args);
-
-		File entryDir = new File(entryDirectorySuffix);
+		File entryDir = new File(dir);
 		if (!entryDir.isDirectory()) {
 			System.err
-					.println(entryDirectorySuffix
+					.println(dir
 							+ " is not a valid directory! Please specify a proper one with -d");
 			System.exit(0);
 		}
 
-		frame = new JFrame("Gnome 3 Application Menu Editor");
+		JFrame frame = new JFrame("Gnome 3 Application Menu Editor");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(
 				"images/icon.png"));
@@ -36,50 +69,5 @@ public class Gnome3MenuEditor {
 		frame.add(new FileListPanel(entryDir));
 		frame.pack();
 		frame.setVisible(true);
-	}
-
-	/**
-	 * Check for and handle terminal commands.
-	 * 
-	 * @param args
-	 *            the command parameters
-	 */
-	public static void checkForTerminalCommands(final String... args) {
-		if (args.length == 0) {
-			try {
-				String username = System.getProperty("user.name");
-				entryDirectorySuffix = "/home/" + username
-						+ entryDirectorySuffix;
-			} catch (final SecurityException e) {
-				System.err
-						.println("Unable to access user.name value! Please supply a directory with -d");
-				System.exit(0);
-			} catch (final NullPointerException e) {
-				System.err
-						.println("No value for the system property user.name! Please supply a directory with -d");
-				System.exit(0);
-			}
-			return;
-		}
-
-		/*
-		 * Help param.
-		 */
-		if (args[0].contains("-h") || args[0].contains("-help")) {
-			System.out
-					.println("This editor allows you to edit the entries in /usr/share/applications/, "
-							+ "which controls the Gnome 3 Applications Menu. "
-							+ "To specify a different directory to parse for entries, "
-							+ "pass it in through command line with '-d'. ex: 'sudo java -jar gnome3-menu-editor -d /home/username/Desktop'.");
-			System.exit(0);
-		}
-
-		if (args[0].contains("-d")) {
-			if (args.length == 1) {
-				System.out.println("Please supply a directory along with -d");
-				System.exit(0);
-			}
-			entryDirectorySuffix = args[1];
-		}
 	}
 }
